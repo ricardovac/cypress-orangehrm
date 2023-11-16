ea4f22b67ef7ba877ba87cb0dca2d35ceb58f412
ricardovac <ricardoviicius@gmail.com>
2023-11-16 19:08:10 -0300

    fix: integration name folder

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
-  video: true,
   e2e: {
     specPattern: "cypress/integration/*.feature",
     setupNodeEvents(on, config) {
